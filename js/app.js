253beaa7655fa236b7bb3df79e36ecc9bed47ee6
Marzuk Zarir <75979581+marzuk-zarir@users.noreply.github.com>
2022-11-02 23:14:26 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 const env = {
-    MAP_ACCESS_TOKEN: 'pk.eyJ1IjoibWFyenVrLXphcmlyIiwiYSI6ImNrcHdsOGF2ajF3dDAycW11ZnplM2lnNXUifQ.EAiefoOGliuh1q0I1kAjaw' /* your access token from https://mapbox.com */,
+    MAP_ACCESS_TOKEN: 'pk.eyJ1IjoibWFyenVrLXphcmlyIiwiYSI6ImNrcHdsOGF2ajF3dDAycW11ZnplM2lnNXUifQ.EAiefoOGliuh1q0I1kAjaw', /* your access token from https://mapbox.com */
     CITY_ACCESS_TOKEN: '2af02de7afba3096c217e360c9d3610f', /* your access token from https://openweathermap.org */
 };
 
-const CITY_URL = 'http://api.openweathermap.org/geo/1.0/direct';
+const CITY_URL = 'https://api.openweathermap.org/geo/1.0/direct';
 const searchForm = document.querySelector('#search-form');
 const cityInput = searchForm.querySelector('[type="text"]');
 
